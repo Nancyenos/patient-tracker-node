0198db16c807339647e6317b3f91bd75708775f7
nancyenos <nicymimz@gmail.com>
2025-01-26 12:44:36 -0800

    feat: add patient model schema

---
+++ models/Patient.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const PatientSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  diagnosis: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Patient", PatientSchema);
